3740d443f0194ac7a798b1af62e9d7ebccfe4aeb
Raphael <raphael.thiney@gmail.com>
2016-06-24 07:27:53 -0400

    added secure site

---
+++ src/WellBellSecure/Data/Migrations/20160624033648_updateduser.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace WellBellSecure.Data.Migrations
+{
+    public partial class updateduser : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "cell",
+                table: "AspNetUsers",
+                nullable: true);
+
+            migrationBuilder.AddColumn<int>(
+                name: "dob",
+                table: "AspNetUsers",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<string>(
+                name: "first",
+                table: "AspNetUsers",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "gender",
+                table: "AspNetUsers",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "last",
+                table: "AspNetUsers",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "nat",
+                table: "AspNetUsers",
+                nullable: true);
+
+            migrationBuilder.AddColumn<int>(
+                name: "registered",
+                table: "AspNetUsers",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<string>(
+                name: "title",
+                table: "AspNetUsers",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "cell",
+                table: "AspNetUsers");
+
+            migrationBuilder.DropColumn(
+                name: "dob",
+                table: "AspNetUsers");
+
+            migrationBuilder.DropColumn(
+                name: "first",
+                table: "AspNetUsers");
+
+            migrationBuilder.DropColumn(
+                name: "gender",
+                table: "AspNetUsers");
+
+            migrationBuilder.DropColumn(
+                name: "last",
+                table: "AspNetUsers");
+
+            migrationBuilder.DropColumn(
+                name: "nat",
+                table: "AspNetUsers");
+
+            migrationBuilder.DropColumn(
+                name: "registered",
+                table: "AspNetUsers");
+
+            migrationBuilder.DropColumn(
+                name: "title",
+                table: "AspNetUsers");
+        }
+    }
+}
---
+++ src/WellBellSecure/Models/ApplicationUser.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+
+namespace WellBellSecure.Models
+{
+    public class Location {
+        public string street { get; set; }
+        public string city { get; set; }
+        public string state { get; set; }
+        public object postcode { get; set; }
+    }
+
+    public class Picture {
+        public string large { get; set; }
+        public string medium { get; set; }
+        public string thumbnail { get; set; }
+    }
+    // Add profile data for application users by adding properties to the ApplicationUser class
+    public class ApplicationUser : IdentityUser
+    {
+        public string gender { get; set; }
+        public string title { get; set; }
+        public string first { get; set; }
+        public string last { get; set; }
+        //public Location location { get; set; }
+ 
+        public int registered { get; set; }
+        public int dob { get; set; }
+ 
+        public string cell { get; set; }
+ 
+      //  public Picture picture { get; set; }
+        public string nat { get; set; }
+    }
+}
